741b837201665fdff85bef3b52b77a0156a16403
Igor Jancev <igor@masterybits.com>
2016-04-14 15:41:43 +0200

    add empty update task

--- lib/railman/cli.rb
+++ lib/railman/cli.rb
@@ -9,5 +9,10 @@ module Railman
     def new(name)
       puts "TODO create new rails application: #{name}"
     end
+
+    desc "update APPNAME", "Update the rails upplication named APPNAME"
+    def update(name)
+      puts "TODO update the rails application: #{name}"
+    end
   end
 end
