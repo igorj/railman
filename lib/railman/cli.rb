a633fb88ea77c506deebefa5e4e20d424c4e5c25
Igor Jancev <igor@masterybits.com>
2016-04-14 21:28:43 +0200

    generate plain rails 4.2.6 application + .ruby-version

--- lib/railman/cli.rb
+++ lib/railman/cli.rb
@@ -5,14 +5,30 @@ module Railman
   class CLI < Thor
     include Thor::Actions
 
+    attr_accessor :app_name
+
+    # this is where the thor generator templates are found
+    def self.source_root
+      File.expand_path('../../../templates', __FILE__)
+    end
+
     desc "new APPNAME", "Create new rails application named APPNAME"
-    def new(name)
-      puts "TODO create new rails application: #{name}"
+    def new(app_name)
+      say "Create a new rails application named: #{app_name}", :green
+      @app_name = app_name
+      @class_name = Thor::Util.camel_case(app_name)
+      directory "rails_app", app_name
+      Dir.chdir app_name do
+      #  create_local_git_repository
+        run "bundle install"
+      #  create_remote_git_repository(@repository)
+      end
+      say "The rails application '#{app_name}' was successfully created.", :green
     end
 
     desc "update APPNAME", "Update the rails upplication named APPNAME"
-    def update(name)
-      puts "TODO update the rails application: #{name}"
+    def update(app_name)
+      puts "TODO update the rails application: #{app_name}"
     end
   end
 end
