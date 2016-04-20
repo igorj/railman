2409d5b28994f1e9bfafb57bde1ac6c6ab9c94b6
Igor Jancev <igor@masterybits.com>
2016-04-20 17:27:24 +0200

    add sidekiq-scheduler gem + configuration

--- templates/rails_app/config/initializers/sidekiq.rb
+++
@@ -1,9 +0,0 @@
-# Configure Sidekiq Redis connection to use the '<%= app_name %>' namespace
-redis_config = { url: 'redis://localhost:6379', namespace: '<%= app_name %>' }
-Sidekiq.configure_server { |config| config.redis = redis_config }
-Sidekiq.configure_client { |config| config.redis = redis_config }
-
-# Load the sidekiq-cron schedule
-#Sidekiq::Cron::Job.load_from_hash! YAML.load_file('config/schedule.yml')
-
-
