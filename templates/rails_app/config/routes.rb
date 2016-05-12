758bdd2d79de75667a1eb3269627fd0eb9237a87
Igor Jancev <igor@masterybits.com>
2016-05-12 20:39:58 +0200

    remove AdminConstraint - move to railman-users engine

--- templates/rails_app/config/routes.rb
+++ templates/rails_app/config/routes.rb
@@ -4,17 +4,8 @@ require 'sidekiq-scheduler/web'
 Sidekiq::Web.set :session_secret, Rails.application.secrets[:secret_token]
 Sidekiq::Web.set :sessions, Rails.application.config.session_options
 
-# used to authenticate the user in sidekiq-web
-class AdminConstraint
-  def matches?(request)
-    return false unless request.session[:user_id]
-    # todo user = User.find request.session[:user_id]
-    # todo user && user.admin?
-  end
-end
-
 Rails.application.routes.draw do
-  mount Sidekiq::Web => '/sidekiq' # todo , :constraints => AdminConstraint.new
+  mount Sidekiq::Web => '/sidekiq'
 
   root 'home#index'
 end
