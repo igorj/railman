1ccf6b348f29a7c924468accce19111dbac69a51
Igor Jancev <igor@masterybits.com>
2016-11-13 08:25:54 +0100

    protect_from_forgery with: :null_session

--- templates/rails_app/app/controllers/application_controller.rb
+++ templates/rails_app/app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
   add_flash_types :success, :error, :warning, :info
 end
