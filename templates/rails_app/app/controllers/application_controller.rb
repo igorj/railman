32607af0fbc3e69fcaf00ea9c3c86aed1dee7850
Igor Jancev <igor@masterybits.com>
2016-11-25 21:24:20 +0100

    add ApplicationController#not_found method

--- templates/rails_app/app/controllers/application_controller.rb
+++ templates/rails_app/app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   add_flash_types :success, :error, :warning, :info
+
+  def not_found
+    raise ActionController::RoutingError.new('Not Found')
+  end
 end
